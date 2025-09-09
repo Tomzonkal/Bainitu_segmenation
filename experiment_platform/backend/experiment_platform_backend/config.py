4659820cf668d01a57d4cafddd7f48aa2744e41f
Filip Hallo <filipha5@wp.pl>
2025-09-09 10:46:52 +0200

    Fixed config

--- experiment_platform/backend/experiment_platform_backend/config.py
+++ experiment_platform/backend/experiment_platform_backend/config.py
@@ -1,10 +1,22 @@
+from pathlib import Path
 
+BASE_DIR = Path(__file__).resolve().parent
 
-RAW_BAINITE_IMAGE_DATASET_PATH="../../../data_labeling/src/raw_images"
-RAW_BAINITE_LABEL_DATASET_PATH="../../../data_labeling/src/metadata"
-RAW_BAINITE_SEGMENTS_DATASET_PATH="../../../data_labeling/src/results"
-RAW_BAINITE_SEGMENTS_LABELS_DATASET_PATH="../../../data_labeling/src/results"
-SUPERPIXEL_BAINITE_SEGMENTS_DATASET_PATH="../../../data_labeling/src/results"
-SUPERPIXEL_BAINITE_SEGMENTS_LABELS_DATASET_PATH="../../../data_labeling/src/results"
+ROOT_DATA_DIR = (BASE_DIR / ".." / ".." / ".." / "data_labeling" / "src").resolve()
+
+RAW_BAINITE_IMAGE_DATASET_PATH = ROOT_DATA_DIR / "raw_images"
+RAW_BAINITE_LABEL_DATASET_PATH = ROOT_DATA_DIR / "metadata"
+RAW_BAINITE_SEGMENTS_DATASET_PATH = ROOT_DATA_DIR / "results"
+RAW_BAINITE_SEGMENTS_LABELS_DATASET_PATH = ROOT_DATA_DIR / "results"
+SUPERPIXEL_BAINITE_SEGMENTS_DATASET_PATH = ROOT_DATA_DIR / "results"
+SUPERPIXEL_BAINITE_SEGMENTS_LABELS_DATASET_PATH = ROOT_DATA_DIR / "results"
+
+# TODO:
+# RAW_BAINITE_IMAGE_DATASET_PATH="../../../data_labeling/src/raw_images"
+# RAW_BAINITE_LABEL_DATASET_PATH="../../../data_labeling/src/metadata"
+# RAW_BAINITE_SEGMENTS_DATASET_PATH="../../../data_labeling/src/results"
+# RAW_BAINITE_SEGMENTS_LABELS_DATASET_PATH="../../../data_labeling/src/results"
+# SUPERPIXEL_BAINITE_SEGMENTS_DATASET_PATH="../../../data_labeling/src/results"
+# SUPERPIXEL_BAINITE_SEGMENTS_LABELS_DATASET_PATH="../../../data_labeling/src/results"
 
 LOGGING_MODE="MLFLOW" # or LOCAL"
